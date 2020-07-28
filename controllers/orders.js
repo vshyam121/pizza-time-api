ebbed480904ad0ddba42f4d30b5b21ec44ef686b
vshyam121 <v.shyam121@gmail.com>
2020-07-28 00:08:02 -0700

    Using Carts collection for orders and getting user id from token

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,31 +1,33 @@
 const User = require('../models/User');
 const Order = require('../models/Order');
+const Cart = require('../models/Cart');
 const ErrorResponse = require('../utils/ErrorResponse');
 
 const asyncHandler = require('../middleware/async');
 
 //@desc     Submit an order
-//@route    POST /orders/:userId
+//@route    POST /orders
 //@access   Private
 exports.submitOrder = asyncHandler(async (req, res, next) => {
   //Find user with corresponding user id
-  let user = await User.findOne(
-    { _id: req.params.userId, 'cart._id': req.body._id },
-    { new: true }
-  ).select('cart');
+  let user = await User.findOne({ _id: req.user._id }, { new: true }).select(
+    'cart'
+  );
 
   //If unable to find user with incoming user id
   if (!user) {
-    return next(
-      new ErrorResponse(
-        `Unable to place an order for user with id of ${req.params.userId}`,
-        400
-      )
-    );
+    return next(new ErrorResponse('Unable to submit order for user', 400));
+  }
+
+  //Get user's cart
+  let cart = await Cart.findOne({ _id: user.cart });
+
+  if (!cart) {
+    return next(new ErrorResponse('Unable to submit order for user', 400));
   }
 
   //Tag on order details to items array to create an order
-  let orderRequest = { items: [...user.cart.items], ...req.body };
+  let orderRequest = { items: cart.items, ...req.body };
 
   //Create the new order in DB
   let order = await Order.create(orderRequest);
@@ -36,13 +38,19 @@ exports.submitOrder = asyncHandler(async (req, res, next) => {
 
   //If successfully created the new cart, then add its id to users order id array
   user = await User.findOneAndUpdate(
-    { _id: req.params.userId },
+    { _id: req.user._id },
     {
-      $set: { cart: { items: [], quantity: 0 } },
       $push: {
         orders: { _id: order._id },
       },
     },
+    { new: true, runValidators: true }
+  );
+
+  //Clear user's cart after successful order
+  cart = await Cart.findOneAndUpdate(
+    { _id: cart._id },
+    { items: [] },
     { new: true, runValidators: true }
   );
 
@@ -51,23 +59,19 @@ exports.submitOrder = asyncHandler(async (req, res, next) => {
 });
 
 //@desc     Get all orders for a user
-//@route    GET /orders/:userId
+//@route    GET /orders
 //@access   Private
 exports.getOrders = asyncHandler(async (req, res, next) => {
   //Get orders array for given user
   let user = await User.findOne(
-    { _id: req.params.userId },
+    { _id: req.user._id },
     {
       'cart.orders': 1,
     }
   ).select('orders');
 
   if (!user) {
-    return next(
-      new ErrorReponse(
-        `Unable to get orders for user with id of ${req.params.userId}`
-      )
-    );
+    return next(new ErrorReponse('Unable to retrieve orders for user', 400));
   }
 
   //Search for ids in orders array in the orders collection
--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,6 @@ const { submitOrder, getOrders } = require('../controllers/orders');
 
 const { protect } = require('../middleware/auth');
 
-router.route('/:userId').get(protect, getOrders).post(protect, submitOrder);
+router.route('/').get(protect, getOrders).post(protect, submitOrder);
 
 module.exports = router;
