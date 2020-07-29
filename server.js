ddc17544b1dc9779a1cd0d79d0f622ead57cf4a0
vshyam121 <v.shyam121@gmail.com>
2020-07-28 18:30:59 -0700

    Default to standard port

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/orders', orders);
 //Middleware to handle common errors
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(
   PORT,
